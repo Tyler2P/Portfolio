67316fefc8855f41f2684cb65edd5be78082a6ae
Tyler <tylerrp06@gmail.com>
2022-04-13 14:02:58 +0100

    Added required files

---
+++ src/webpack.config.js
@@ -0,0 +1,23 @@
+const terserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    module: {
+        loaders: [
+            // {
+            //     test: /\.css$/,
+            //     loader: "style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]"
+            // },
+            { test: /\.css$/, loader: "style-loader!css-loader" }
+        ],
+        rules: [{
+            test: /\.css$/,
+            use: [ "style-loader", "css-loader" ]
+        }]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new terserPlugin({ parallel: true, minify: true })
+        ]
+    }
+}
